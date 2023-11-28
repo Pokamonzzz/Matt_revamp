c9e806c585fb54d5b3fa94280a2f2d45b4e60b9b
Pokamonzzz <mattiastan@gamil.com>
2023-11-29 00:14:22 +0800

    still trying to do full overlay of dropdown menu

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+}
+  
+  /* Close */
+  function closeNav(){
+    document.getElementById("myNav").style.height = "0%";
+}
 function opennav(){
     document.getElementById("dropdown").classList.toggle("show")
 }
