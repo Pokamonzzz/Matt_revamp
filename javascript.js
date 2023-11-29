8f03edf7b49cf6e349aa47a1ca683f8bb7f0b6cf
Pokamonzzz <mattiastan@gamil.com>
2023-11-29 13:49:59 +0800

    dropdown menu doesnt work :(

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-function openNav(){
-    document.getElementById("myNav").style.height = "100%";
-}
-  
-  /* Close */
-  function closeNav(){
-    document.getElementById("myNav").style.height = "0%";
-}
+
 function opennav(){
     document.getElementById("dropdown").classList.toggle("show")
+}
+function openNav() {
+    document.getElementById("mySidenav").style.height = "100%";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
 }
