7fc51696eaa498d6c2a9d4219e8f90071a73b53b
Pokamonzzz <mattiastan@gamil.com>
2023-11-29 14:30:22 +0800

    dropdown worked. Now to make it full screen

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
-
-function opennav(){
-    document.getElementById("dropdown").classList.toggle("show")
-}
-function openNav() {
-    document.getElementById("mySidenav").style.height = "100%";
+function toggleNav() {
+    var dropdown = document.getElementById("mySidenav");
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
 }
 function closeNav() {
-    document.getElementById("mySidenav").style.height = "0";
+    var dropdown = document.getElementById("mySidenav");
+    dropdown.style.display = "none";
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.dpmenu') && !event.target.closest('.menu')) {
+        var dropdown = document.getElementById("mySidenav");
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none";
+        }
+    }
 }
